95f0295c5dbb536e87fa184579dfcf3d489f7204
scorp18pl <69383014+scorp18pl@users.noreply.github.com>
2022-05-14 19:39:03 +0200

    Added count input

--- PNoise_Map_Generator/Main.cpp
+++ PNoise_Map_Generator/Main.cpp
@@ -1,9 +1,15 @@
 #include "MapGen.h"
 
+#include <iostream>
+
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	for (;;) {
+	size_t map_count;
+	std::cout << "Insert map count" << std::endl;
+	std::cin >> map_count;
+
+	for (int i = 0; i < map_count; i++) {
 		MapGen m(1080, 1080, 0.6f, true);
 		//m.setSeed(4);
 		m.setSeaLevel(0.52f);
